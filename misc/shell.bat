89fb25b077113ad67a3f8c13c4e171141ec00cfb
Erik Buchholz <ebuchholz17@gmail.com>
2018-12-19 13:11:47 -0500

    removed command prompt/shell.bat

--- misc/shell.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
-set path=C:\Users\erik.buchholz\Downloads\advent_of_code_2018\misc;%path%
